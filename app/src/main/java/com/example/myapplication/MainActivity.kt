a5d5cb9aef480cb39d2b1bd8a39edc976b22b79d
Lavan Nithianandarajah <lavannithi2@gmail.com>
2024-01-19 00:14:21 -0500

    Vertical Scroll on dropDownMenu

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,18 +8,12 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -73,17 +67,18 @@ fun DropDownMenu(buildings: Map<String, Buildings>) {
     val context = LocalContext.current
     var expanded by remember { mutableStateOf(false) }
     var selectedText by remember { mutableStateOf(buildings["Davis Center"]?.name ?:" ")}
-
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(32.dp)
     ) {
         ExposedDropdownMenuBox(
+            modifier = Modifier.verticalScroll(rememberScrollState()),
             expanded = expanded,
             onExpandedChange = {
                 expanded = !expanded
-            }
+            },
+
         ) {
             TextField(
                 value = selectedText,
