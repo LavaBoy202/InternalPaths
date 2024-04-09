b155a1909f6d2dcec893eb2c4e34934354943b92
Lavan Nithi <lavannithi2@gmail.com>
2024-04-09 12:46:40 -0400

    Style the Screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,9 +12,14 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+
+
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -23,8 +28,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,33 +44,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
                     Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
+                            .fillMaxWidth(),
                         horizontalAlignment = Alignment.CenterHorizontally
-
                     ) {
                         // Top Dropdown Menu
-
                         // Greeting Text
-                        Column(modifier = Modifier.fillMaxWidth().clip(shape = RoundedCornerShape(20.dp)).clip(RoundedCornerShape(10.dp))
+                        Column(modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(20.dp))
+                            .clip(RoundedCornerShape(10.dp))
                             .background(Color(0xFF000000), shape = RoundedCornerShape(15.dp))
-                            .height(300.dp)){
-                            Text(text = "UNIVERSITY", fontSize = 40.sp, fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, color = Color(0xFFFFD166))
-                            Text(text = "OF", fontSize = 40.sp, fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, color = Color(0xFFFFD166))
-                            Text(text = "WATERLOO", fontSize = 40.sp, fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, color = Color(0xFFFFD166))
+                            .height(400.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center){
+                            Text(text = "UNIVERSITY", fontSize = 44.sp, fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, color = Color(0xFFFFD166))
+                            Text(text = "OF", fontSize = 44.sp, fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, color = Color(0xFFFFD166))
+                            Text(text = "WATERLOO", fontSize = 44.sp, fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, color = Color(0xFFFFD166))
                         }
-                        Column(modifier = Modifier.fillMaxWidth().clip(shape = RoundedCornerShape(20.dp)).clip(RoundedCornerShape(10.dp))
+                        Column(modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(20.dp))
+                            .clip(RoundedCornerShape(10.dp))
                             .background(Color(0xFF878472), shape = RoundedCornerShape(15.dp))
-                            .height(300.dp)){
-                            DropDownMenu(buildings = initBuildings())
-
-
-                            DropDownMenu(buildings = initBuildings())
-
-
+                            .fillMaxHeight()){
+                            DropDownMenu(buildings = initBuildings(), 1)
+                            DropDownMenu(buildings = initBuildings(), 2)
                             FilledTonalButtonExample()
                         }
 
@@ -78,45 +86,68 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun FilledTonalButtonExample() {
     var buttonText by remember { mutableStateOf("Submit") }
-    FilledTonalButton(onClick = {
-        val startBuilding = BuildingE.DC.building
-        val targetBuilding = BuildingE.M3.building
-        val Buildings = initBuildings()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        FilledTonalButton(
+            onClick = {
+                val startBuilding = BuildingE.DC.building
+                val targetBuilding = BuildingE.M3.building
+                val Buildings = initBuildings()
 
-        val pathExists = PathExists(BuildingE.DC.building, BuildingE.M3.building, Buildings)
-        buttonText = pathExists.toString()
-        Log.i("PathExists", "Path exists between $startBuilding and $targetBuilding: $pathExists")
-    }, colors = ButtonDefaults.buttonColors(containerColor = Color.Black)) {
-        Text(buttonText)
+                val pathExists = PathExists(BuildingE.DC.building, BuildingE.M3.building, Buildings)
+                buttonText = pathExists.toString()
+                Log.i("PathExists", "Path exists between $startBuilding and $targetBuilding: $pathExists")
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFD166), contentColor = Color.Black)
+        ) {
+            Text(buttonText)
+        }
     }
 }
 
 @Composable
-fun DropDownMenu(buildings: Map<String, Buildings>) {
+fun DropDownMenu(buildings: Map<String, Buildings>, number: Int) {
     val context = LocalContext.current
     var expanded by remember { mutableStateOf(false) }
-    var selectedText by remember { mutableStateOf(buildings["Davis Center"]?.name ?:"")}
+    var selectedText by remember { mutableStateOf(buildings["Davis Center"]?.name ?:"FROM")}
+    var selectedText2 by remember { mutableStateOf(buildings["Davis Center"]?.name ?:"TO")}
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(0.dp, 32.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
+        horizontalArrangement = Arrangement.Center,
+
     ) {
         ExposedDropdownMenuBox(
-            modifier = Modifier.verticalScroll(rememberScrollState()).background(Color.Blue),
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .background(Color.Blue),
             expanded = expanded,
             onExpandedChange = {
                 expanded = !expanded
             },
 
         ) {
+
+            Icon(
+                imageVector = Icons.Filled.AccountCircle,
+                contentDescription = "Localized description",
+                modifier = Modifier.size(50.dp)
+
+            )
             TextField(
-                value = selectedText,
+                value = if(number == 1) selectedText else selectedText2,
                 onValueChange = {},
                 readOnly = true,
                 trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                modifier = Modifier.menuAnchor()
+                colors = ExposedDropdownMenuDefaults.textFieldColors(containerColor = Color(0xFFFFD166),),
+                modifier = Modifier.menuAnchor(),
+                textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
             )
 
             ExposedDropdownMenu(
